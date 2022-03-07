b6c7fa499d7dc4dc257dd53ec4fa8c05e8539a99
Anil Kumar Pandiya <anilpandiya05@gmail.com>
2022-03-07 18:38:13 +0530

    code clean up

--- js/app.js
+++ js/app.js
@@ -87,41 +87,4 @@ $(document).ready(function () {
       }
     }
   });
-
-  // $('input[type="text"]').keypress(function (event) {
-  //   const email = $('input[type="text"]').val().toLowerCase();
-  //   const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  //   if (email.match(regEx)) {
-  //     x = true;
-  //     document.querySelector('input[type="text"]').parentNode.classList.remove("error");
-  //   } else {
-  //     x = false;
-  //   }
-  //   let keycode = (event.keyCode ? event.keyCode : event.which);
-  //   if (keycode === '13') {
-  //     /**
-  //      * Makes a request to ltv API to search an specific email address.
-  //      * If there's a response, it gets stored in the local storage and redirects to results page
-  //      */
-  //     event.preventDefault();
-  //     localStorage.clear(); //Clears storage for next request
-
-  //     let x;
-
-  //     if (x === true) {
-  //       const proxyurl = "";
-  //       const url =
-  //         'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
-  //       fetch(proxyurl + url)
-  //         .then((response) => response.text())
-  //         .then(function (contents) {
-  //           localStorage.setItem("userObject", contents);
-  //           window.location.href = "result.html";
-  //         })
-  //         .catch((e) => console.log(e));
-  //     } else if (x !== true) {
-  //       document.querySelector('input[type="text"]').parentNode.classList.add("error");
-  //     }
-  //   }
-  // });
 });
